0d4e4117691cf7a4c03f3578f5c2afc87da4ecd6
AlwennaCochard <alwenna.cochard@etudiant.univ-brest.fr>
2021-04-19 16:35:37 +0200

    compile script working

--- TextualPart/JSListener.js
+++ TextualPart/JSListener.js
@@ -33,34 +33,66 @@ JSListener.prototype.exitInstantiation = function(ctx) {
 
 };
 
+
+class getNodeType{
+    constructor(id, id2)
+    {
+        this.id = new getId(id);
+        this.id2 = new getComponant(id2);
+    }
+}
 JSListener.prototype.exitNode_type = function(ctx) {
-  
+    const myNodeType = new getNodeType(ctx.getChild(1), ctx.getChild(3))
 };
 
-JSListener.prototype.exitRelationship_type = function(ctx) {
-};
 
+JSListener.prototype.exitRelationship_type = function(ctx) {};
+
+
+class getNode{
+    constructor(id, comp)
+    {
+        this.id = new getId(id);
+        this.comp = new getComponant(comp);
+    }
+}
 JSListener.prototype.exitNode = function(ctx) {
-   
+   const myNode = new getNode(ctx.getChild(1), ctx.getChild(3))
 };
 
-JSListener.prototype.exitRelationship = function(ctx) {
-    
-};
 
+JSListener.prototype.exitRelationship = function(ctx) {};
+
+
+class getNumber {
+    constructor(num) {
+        this.num = num;
+    }
+}
 JSListener.prototype.exitNumber = function(ctx) {
-    
+    const myNum = new getNumber(ctx.getChild(0));
 };
 
-JSListener.prototype.exitComment = function(ctx) {
-    
-};
 
+JSListener.prototype.exitComment = function(ctx) {};
+
+
+class getId {
+    constructor(name) {
+        this.name = name;
+    }
+}
 JSListener.prototype.exitId = function(ctx) {
-    
+    const myId = new getId(ctx.getChild(0));
 };
 
+
+class getComponant {
+    constructor(comp) {
+        this.comp = comp;
+    }
+}
 JSListener.prototype.exitComponant = function(ctx) {
-    
+    const myComponant = new getComponant(ctx.getChild(0));
 };
 
