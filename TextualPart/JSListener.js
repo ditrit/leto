43e24e03384413b8335f4033d08f37911df9e74c
AlwennaCochard <alwenna.cochard@etudiant.univ-brest.fr>
2021-04-21 03:42:55 +0200

    compile script working

--- TextualPart/JSListener.js
+++ TextualPart/JSListener.js
@@ -25,11 +25,7 @@ JSListener.prototype.exitInstruction = function(ctx) {
 
 JSListener.prototype.exitDefinition = function(ctx) {
     ctx.model = ctx.getChild(0).model
-<<<<<<< HEAD
     console.log("Création de l'id : " + ctx.model);
-=======
-    console.log("Création de l'id : " + ctx.model)
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
 };
 
 JSListener.prototype.exitInstantiation = function(ctx) {
@@ -59,10 +55,7 @@ JSListener.prototype.exitRelationship = function(ctx) {};
 
 JSListener.prototype.exitNumber = function(ctx) {
     ctx.model = new model.Number(ctx.getChild(0));
-<<<<<<< HEAD
     console.log("Création du nombre : " + ctx.model);
-=======
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
 };
 
 
@@ -71,29 +64,11 @@ JSListener.prototype.exitComment = function(ctx) {};
 
 JSListener.prototype.exitId = function(ctx) {
     ctx.model = new model.Id(ctx.getChild(0));
-<<<<<<< HEAD
     console.log("Création de l'id : " + ctx.model);
-=======
-    console.log("Création de l'id : " + ctx.model)
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
 };
 
 
 JSListener.prototype.exitComponant = function(ctx) {
-<<<<<<< HEAD
     ctx.model = new.model.Componant(ctx.getChild(0));
     console.log("Création du composant choisi : " + ctx.model);
 };
-=======
-    idComponant = ctx.getChild(0).model
-    idParent = ctx.getChild(1) 
-    if (idParent != null) {
-        idParent = idParent.model
-    } else {
-        idParent = null
-    }
-    ctx.model = new model.Componant(idComponant, idParent);
-    console.log("Création du composant : " + ctx.model)
-};
-
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
