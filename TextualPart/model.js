2d402fd586b73349f8b536d8d7ae257d97e1e935
AlwennaCochard <alwenna.cochard@etudiant.univ-brest.fr>
2021-04-21 03:44:12 +0200

    compile script working

--- TextualPart/model.js
+++ TextualPart/model.js
@@ -8,7 +8,6 @@ class Model {
 class Componant extends Model {
     constructor(Id, IdParent) {
         super(Id.name, "componant");
-<<<<<<< HEAD
         this.Id = Id;
         this.parent = IdParent;
     }
@@ -17,10 +16,6 @@ class Componant extends Model {
 class Componant extends Model {
     constructor(componant) {
         super(componant, "componant")
-=======
-        this.Id = Id
-        this.parent = IdParent
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
     }
 }
 
@@ -28,13 +23,10 @@ class Id extends Model {
     constructor(id) {
         super(id, "id")
     }
-<<<<<<< HEAD
 }
 
 class Number extends Model {
     constructor(number) {
         super(number, "number")
     }
-=======
->>>>>>> 5fbc0f7459abd59cf214db087b3f0252ec5fca6a
 }
