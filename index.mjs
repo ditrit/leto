99a8b3040a3cdc2ab45a652eafe132bb0dd7a25b
AlwennaCochard <alwenna.cochard@etudiant.univ-brest.fr>
2021-04-27 10:12:15 +0200

    update MyLetoListener & model

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
 import { parse } from "./TextualPart/index.js";
 
-/*
 console.log('test :')
-parse(`componant toto from serv {logo : test/test.txt; host : test}; 
+parse(`componant toto from serv {logo : test/test.txt; host : test};  
+componant tutu {logo : test/test.txt; host : test, test2};
 relationship titi from tata
 link tata - test;
+link tutu - serv;
 asset bdd : titi`)
-
+/*
 console.log('test 2 :')
 parse(`componant toto from serv {}`)
 
@@ -18,7 +19,10 @@ parse(`componant toto {};`)
 
 console.log('test 5 :')
 parse(`componant toto {logo : test/test.txt; host : test}; link toto - test; asset bdd : titi`)
-*/
 
-console.log('test 3 :')
+console.log('test 6 :')
 parse(`componant toto {logo : test/test.txt; host : test, test2};`)
+
+console.log('test 7 :')
+parse(`componant test {}; asset testi : toto`)
+*/
