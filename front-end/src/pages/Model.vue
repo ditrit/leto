dea8128fa54f9e3a0f97e88302c61e36e1d339ac
BS <bs@galaxy-s10.home>
2021-07-08 16:31:35 +0200

    Finish the sidebar for both pages

--- front-end/src/pages/Model.vue
+++ front-end/src/pages/Model.vue
@@ -1,117 +1,131 @@
 <template>
-  <div>
-    <q-layout
-      view="hHh Lpr lff"
-      container
-      style="height: 300px"
-      class="shadow-2 rounded-borders"
+  <q-layout container class="global">
+    <q-toolbar>
+      <div class="row">
+        <q-btn
+          flat
+          @click="drawer = !drawer"
+          round
+          dense
+          icon="menu"
+          :class="drawer ? 'menuStyle hiddenMenu' : ' menuStyle visibleMenu'"
+        />
+      </div>
+    </q-toolbar>
+    <q-drawer
+      v-model="drawer"
+      @hide="makeMenuVisible"
+      show-if-above
+      @click.capture="drawerClick"
+      :width="250"
+      :breakpoint="500"
+      bordered
+      class="bg-grey-3"
     >
-      <q-header elevated class="bg-black">
-        <q-toolbar>
-          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
-          <q-toolbar-title>Model</q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-
-      <q-drawer
-        v-model="drawer"
-        show-if-above
-        :mini="!drawer || miniState"
-        @click.capture="drawerClick"
-        :width="200"
-        :breakpoint="500"
-        bordered
-        class="bg-grey-3"
-      >
-        <q-scroll-area class="fit">
-          <q-list padding>
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="inbox" />
-              </q-item-section>
-
-              <q-item-section> Inbox </q-item-section>
-            </q-item>
-
-            <q-item active clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="star" />
-              </q-item-section>
-
-              <q-item-section> Star </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="send" />
-              </q-item-section>
-
-              <q-item-section> Send </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="drafts" />
-              </q-item-section>
-
-              <q-item-section> Drafts </q-item-section>
-            </q-item>
-          </q-list>
-        </q-scroll-area>
-
-        <!--
-          in this case, we use a button (can be anything)
-          so that user can switch back
-          to mini-mode
-        -->
-        <div
-          class="q-mini-drawer-hide absolute"
-          style="top: 15px; right: -17px"
+      <q-list v-for="item in sidebarItems" :key="item.id">
+        <q-expansion-item
+          group="somegroup"
+          :icon="item.icon"
+          :label="item.caption"
+          default-opened
+          header-class="text-primary"
+          style="font-weight: 600"
         >
-          <q-btn
-            dense
-            round
-            unelevated
-            color="accent"
-            icon="chevron_left"
-            @click="miniState = true"
-          />
-        </div>
-      </q-drawer>
+          <q-item
+            v-for="child in item.children"
+            :key="child.id"
+            clickable
+            v-ripple
+          >
+            <q-item-section avatar class="q-pl-md">
+              <q-icon color="grey" :name="child.icon" size="16px" />
+            </q-item-section>
+            <q-item-section style="color: grey">{{
+              child.caption
+            }}</q-item-section>
+          </q-item>
+        </q-expansion-item>
+        <q-sepaartor />
+      </q-list>
+    </q-drawer>
 
-      <q-page-container>
-        <q-page class="q-px-lg q-py-md">
-          <h1>the Model viewer</h1>
-        </q-page>
-      </q-page-container>
-    </q-layout>
-  </div>
+    <q-page-container>
+      <q-page class="q-px-lg q-py-md">
+        <h1>the Model viewer</h1>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
 import { ref } from "vue";
 
+const sidebarItems = [
+  {
+    id: 1,
+    caption: "Servers",
+    icon: "dns",
+    children: [
+      { id: 1, caption: "Serveur de fichier", icon: "folder" },
+      { id: 2, caption: "Serveur d’impression", icon: "print" },
+      { id: 3, caption: "Serveur d’application", icon: "apps" },
+    ],
+  },
+  {
+    id: 2,
+    itle: "",
+    caption: "Routers",
+    icon: "router",
+    children: [
+      { id: 1, caption: "Element one", icon: "router" },
+      { id: 2, caption: "Element two", icon: "router" },
+      { id: 3, caption: "Element three", icon: "router" },
+    ],
+  },
+  {
+    id: 3,
+    caption: "Database",
+    icon: "storage",
+    children: [
+      { id: 1, caption: "Element one", icon: "storage" },
+      { id: 2, caption: "Element two", icon: "storage" },
+      { id: 3, caption: "Element three", icon: "storage" },
+    ],
+  },
+  {
+    id: 4,
+    caption: "Network",
+    icon: "wifi",
+    children: [
+      { id: 1, caption: "Element one", icon: "wifi" },
+      { id: 2, caption: "Element two", icon: "wifi" },
+      { id: 3, caption: "Element three", icon: "wifi" },
+    ],
+  },
+];
+
 export default {
   setup() {
-    const miniState = ref(false);
-
     return {
       drawer: ref(false),
-      miniState,
-
-      drawerClick(e) {
-        // if in "mini" state and user
-        // click on drawer, we switch it to "normal" mode
-        if (miniState.value) {
-          miniState.value = false;
-
-          // notice we have registered an event with capture flag;
-          // we need to stop further propagation as this click is
-          // intended for switching drawer to "normal" mode only
-          e.stopPropagation();
-        }
-      },
+      sidebarItems,
     };
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.global
+  width: 100%
+  height: 100%
+  overflow: hidden
+  min-height: 1200px
+.menuStyle
+  background: #eeeeee
+  border-radius: 0
+  margin-top: -16px
+.hiddenMenu
+  transform: translateX(238px)
+.visibleMenu
+  transform: translateX(-14px)
+</style>
