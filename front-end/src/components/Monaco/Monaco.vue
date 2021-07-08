ebe38ab68194ec2bad8756497633466ba8568ff6
BS <bs@galaxy-s10.home>
2021-07-08 18:04:21 +0200

    issues with vue-monaco --> not working with quasar v3

---
+++ front-end/src/components/Monaco/Monaco.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    hello monaco
+    <MonacoEditor
+      width="800"
+      height="500"
+      theme="vs-dark"
+      language="javascript"
+      :options="options"
+      @change="onChange"
+    ></MonacoEditor>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from "monaco-editor-vue";
+export default {
+  name: "App",
+  components: {
+    MonacoEditor,
+  },
+  data() {
+    return {
+      options: {
+        //Monaco Editor Options
+      },
+    };
+  },
+  methods: {
+    onChange(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
