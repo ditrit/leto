1fea2030a45ccebb944dbbcaab452cd328d072b5
BrahimS <bsourny@gmail.com>
2021-09-15 12:07:54 +0200

    fix issues related to stepper submit form

--- frontend/src/components/Stepper/CreationFormStepper.vue
+++ frontend/src/components/Stepper/CreationFormStepper.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="q-pa-md">
-		<q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+		<q-form
+			@submit="step === 3 ? onSubmit() : $refs.stepper.next()"
+			class="q-gutter-md"
+		>
 			<q-stepper v-model="step" ref="stepper" animated active-color="purple">
 				<q-step :name="1" prefix="1" title="">
 					<div class="row">
@@ -60,24 +63,92 @@
 					</div>
 				</q-step>
 
-				<q-step :name="2" prefix="2" title=""> </q-step>
+				<q-step :name="2" prefix="2" title="">
+					<div class="row">
+						<div class="col col-md-4 q-mr-md">
+							<q-input
+								filled
+								label="Name *"
+								hint=""
+								lazy-rules
+								:rules="[
+									(val) => (val && val.length > 0) || 'Please type something',
+								]"
+								v-model="name"
+							/>
+						</div>
+						<div class="col col-md-4 q-mr-md">
+							<q-select
+								filled
+								:options="options"
+								label="Team Parent"
+								v-model="teamParent"
+							/>
+						</div>
+						<div class="col col-md-3">
+							<q-uploader
+								style="max-width: 192px"
+								url="http://localhost:8080/upload"
+								label="Logo"
+								multiple
+								accept=".jpg, svg, image/*"
+								@rejected="onRejected"
+							/>
+						</div>
+					</div>
+					<div class="row q-mt-md">
+						<div class="col col-md-12">
+							<q-input
+								filled
+								label="Short description"
+								lazy-rules
+								:rules="[
+									(val) => (val && val.length > 0) || 'Please type something',
+								]"
+								v-model="shortDescription"
+							/>
+						</div>
+					</div>
+					<div class="row q-mt-md">
+						<div class="col col-md-12">
+							<q-input
+								filled
+								type="textarea"
+								label="Description"
+								v-model="description"
+							/>
+						</div>
+					</div>
+				</q-step>
 
-				<q-step :name="3" prefix="3" title=""> </q-step>
+				<q-step :name="3" prefix="3" title="">
+					<div class="row q-mt-md">
+						<div class="col col-md-12">thank you</div>
+					</div>
+				</q-step>
 
 				<template v-slot:navigation>
 					<q-stepper-navigation>
 						<div class="flex justify-center">
 							<q-btn
-								type=""
+								v-if="step <= 2"
 								@click="$refs.stepper.next()"
 								color="positive"
-								:label="step === 3 ? 'Send' : 'Next'"
+								label="Next"
 								style="padding: 15px 50px"
 							/>
 							<q-btn
+								v-if="step > 2"
+								type=""
+								@click="$refs.stepper.close()"
+								color="positive"
+								label="Ok"
+								style="padding: 15px 50px"
+							/>
+
+							<q-btn
 								v-if="step > 1"
-								filled
-								color="grey"
+								unelevated
 								@click="$refs.stepper.previous()"
 								label="Back"
 								class="q-ml-sm"
@@ -101,6 +172,7 @@ export default {
 		const teamParent = ref("");
 		const shortDescription = ref("");
 		const description = ref("");
+		const test1 = ref("");
 
 		const $q = useQuasar();
 		const text = ref("");
@@ -122,6 +194,7 @@ export default {
 			teamParent,
 			shortDescription,
 			description,
+			test1,
 
 			onSubmit() {
 				const newDomain = {
@@ -129,9 +202,14 @@ export default {
 					teamParent: teamParent.value,
 					shortDescription: shortDescription.value,
 					description: description.value,
+					test1: test1.value,
 				};
 				store.dispatch("appDomain/addDomain", newDomain);
 				console.log(newDomain);
+				$q.notify({
+					type: "positive",
+					message: "Your data was sent ",
+				});
 			},
 		};
 	},
@@ -140,6 +218,8 @@ export default {
 <style lang="sass">
 .q-card
 	width: 800px !important
+
+	min-height: fill-available !important
 .q-stepper
 	width: 720px !important
 	box-shadow: none !important
