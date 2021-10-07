a7100c9be223bf1f820757aee78be0fb9695e141
BrahimS <bsourny@gmail.com>
2021-10-07 18:01:14 +0200

    Layout domain page -- second part -- + Create Card components for content and tags + Add drawer to  domain layout

--- frontend/src/components/Ui/TabPanels/Tabs.vue
+++ frontend/src/components/Ui/TabPanels/Tabs.vue
@@ -1,65 +1,75 @@
 <template>
-	<div class="q-pa-md">
-		<div class="col col-md-12" style="max-width: 600px">
-			<q-card>
-				<q-tabs
-					v-model="tab"
-					class="text-grey"
-					active-color="primary"
-					indicator-color="primary"
-					align="justify"
-					narrow-indicator
-				>
-					<q-tab name="tags" label="Tags" />
-					<q-tab name="team_members" label="Team members" />
-					<q-tab name="libraries" label="Libraries" />
-					<q-tab name="environnements" label="Environnements" />
-				</q-tabs>
-				<q-separator />
+	<div class="col col-md-12">
+		<q-card>
+			<q-tabs
+				v-model="tab"
+				class="text-grey"
+				active-color="primary"
+				indicator-color="primary"
+				align="justify"
+				narrow-indicator
+			>
+				<q-tab dense name="tags" label="Tags" icon="sell" />
+				<q-tab dense name="products" label="Products" icon="apps" />
+				<q-tab dense name="team_members" label="Team members" icon="group" />
+				<q-tab dense name="libraries" label="Libraries" icon="library_books" />
+				<q-tab dense name="environnements" label="Environnements" icon="code" />
+			</q-tabs>
+			<q-separator />
 
-				<q-tab-panels v-model="tab" animated>
-					<q-tab-panel name="tags">
-						<ul class="cards_wrapper">
-							<li v-for="(tag, index) in allTags" :key="index">{{ tag }}</li>
-						</ul>
-					</q-tab-panel>
+			<q-tab-panels v-model="tab" animated>
+				<q-tab-panel name="tags">
+					<ul class="cards_wrapper">
+						<li v-for="(tag, index) in allTags" :key="index">{{ tag }}</li>
+					</ul>
+				</q-tab-panel>
 
-					<q-tab-panel name="team_members">
-						<div class="cards_wrapper q-gutter-x-sm">
-							<ActionCard
-								v-for="member in teamMembers"
-								:key="member.id"
-								:name="member.name"
-								:role="member.role"
-								:description="member.description"
-							/>
-						</div>
-					</q-tab-panel>
+				<q-tab-panel name="products">
+					<div class="cards_wrapper q-gutter-x-sm">
+						<ActionCard
+							v-for="product in teamProducts"
+							:key="product.id"
+							:name="product.name"
+							:role="product.role"
+							:description="product.description"
+						/>
+					</div>
+				</q-tab-panel>
+				<q-tab-panel name="team_members">
+					<div class="cards_wrapper q-gutter-x-sm">
+						<ActionCard
+							v-for="member in teamMembers"
+							:key="member.id"
+							:name="member.name"
+							:role="member.role"
+							:description="member.description"
+						/>
+					</div>
+				</q-tab-panel>
 
-					<q-tab-panel name="libraries">
-						<div class="cards_wrapper q-gutter-x-sm">
-							<ActionCard
-								v-for="librarie in teamLibraries"
-								:key="librarie.id"
-								:name="librarie.name"
-								:description="librarie.description"
-							/>
-						</div>
-					</q-tab-panel>
+				<q-tab-panel name="libraries">
+					<div class="cards_wrapper q-gutter-x-sm">
+						<ActionCard
+							v-for="librarie in teamLibraries"
+							:key="librarie.id"
+							:name="librarie.name"
+							:description="librarie.description"
+						/>
+					</div>
+				</q-tab-panel>
 
-					<q-tab-panel name="environnements">
-						<div class="cards_wrapper q-gutter-x-sm">
-							<ActionCard
-								v-for="env in teamEnvironnements"
-								:key="env.id"
-								:name="env.name"
-								:description="env.description"
-							/>
-						</div>
-					</q-tab-panel>
-				</q-tab-panels>
-			</q-card>
-		</div>
+				<q-tab-panel name="environnements">
+					<div class="cards_wrapper q-gutter-x-sm">
+						<ActionCard
+							v-for="env in teamEnvironnements"
+							:key="env.id"
+							:name="env.name"
+							:description="env.description"
+						/>
+					</div>
+				</q-tab-panel>
+			</q-tab-panels>
+		</q-card>
 	</div>
 </template>
 <script>
@@ -95,6 +105,29 @@ export default {
 					logo: "https://cdn.quasar.dev/img/parallax2.jpg",
 					name: "Sophia",
 					role: "DevOps",
+					description: "Ceci est une description",
+				},
+			],
+		},
+		teamProducts: {
+			type: [],
+			default: [
+				{
+					id: 0,
+					logo: "https://cdn.quasar.dev/img/parallax2.jpg",
+					name: "Product 1",
+					description: "Ceci est une description",
+				},
+				{
+					id: 1,
+					logo: "https://cdn.quasar.dev/img/parallax2.jpg",
+					name: "Product 2",
+					description: "Ceci est une description",
+				},
+				{
+					id: 2,
+					logo: "https://cdn.quasar.dev/img/parallax2.jpg",
+					name: "Product 3",
 					description: "Ceci est une description",
 				},
 			],
--- frontend/src/pages/PageTeamChild.vue
+++ frontend/src/pages/PageTeamChild.vue
@@ -1,24 +1,88 @@
 <template>
 	<q-layout>
 		<AjaxBar />
-		<q-page padding class="content_wrapper bg-gray q-mt-lg">
-			<div>
+		<q-page padding class="bg-gray">
+			<q-toolbar>
+				<div class="row">
+					<q-btn
+						flat
+						@click="drawer = !drawer"
+						round
+						dense
+						icon="menu"
+						:class="drawer ? 'menuStyle hiddenMenu' : ' menuStyle visibleMenu'"
+					/>
+				</div>
+			</q-toolbar>
+			<q-drawer
+				v-model="drawer"
+				@hide="makeMenuVisible"
+				show-if-above
+				@click.capture="drawerClick"
+				:width="250"
+				:breakpoint="500"
+				bordered
+				class="main_drawer bg-grey-3"
+			>
+				<div class="q-mt-md" style="max-width: 350px">
+					<div class="q-gutter-md">
+						<q-input
+							v-model="search"
+							debounce="500"
+							placeholder="Search"
+							class="search_input q-mb-lg q-pa-md"
+						>
+							<template v-slot:append>
+								<q-icon name="search" />
+							</template>
+						</q-input>
+					</div>
+				</div>
+				<q-list v-for="item in filterdSidebarItem" :key="item.id">
+					<q-expansion-item
+						group="somegroup"
+						:icon="item.icon"
+						:label="item.caption"
+						:data-id="item.attr"
+						default-opened
+						header-class="text-primary"
+						style="font-weight: 600"
+					>
+						<q-item
+							v-for="child in item.children"
+							:key="child.id"
+							:data-id="child.attr"
+							clickable
+							v-ripple
+						>
+							<q-item-section avatar class="q-pl-md">
+								<q-icon color="grey" :name="child.icon" size="16px" />
+							</q-item-section>
+							<q-item-section style="color: grey">{{
+								child.caption
+							}}</q-item-section>
+						</q-item>
+					</q-expansion-item>
+					<q-sepaartor />
+				</q-list>
+			</q-drawer>
+			<div class="content_wrapper">
 				<q-card
 					class="card_default"
 					flat
 					bordered
-					v-for="item in dataItems"
+					v-for="item in child"
 					:key="item.id"
 				>
 					<q-card-section>
 						<div class="row no-wrap">
 							<div class="col">
 								<div class="text-h6">
-									<q-icon :name="item.child.icon" size="30px" class="q-mr-sm" />
-									{{ item.child.headline }}
+									<q-icon :name="item.icon" size="30px" class="q-mr-sm" />
+									{{ item.headline }}
 								</div>
 								<div class="text-subtitle3 text-grey-8">
-									{{ item.child.subTitle }}
+									{{ item.subTitle }}
 								</div>
 								<div class="content_wrapper q-mt-md">
 									<img
@@ -26,14 +90,14 @@
 										alt="logo"
 									/>
 									<p class="q-ml-md">
-										{{ item.child.textContent }}
+										{{ item.textContent }}
 									</p>
 								</div>
 							</div>
 
 							<div class="col-auto">
 								<q-btn color="grey-7" round flat icon="more_vert">
-									<q-menu cover auto-close>
+									<q-menu cover auto-close class="card_actions">
 										<q-list>
 											<q-item clickable>
 												<q-item-section>Link One</q-item-section>
@@ -51,40 +115,53 @@
 						</div>
 					</q-card-section>
 				</q-card>
-			</div>
-			<div class="tags_wrapper">
-				<q-card flat bordered class="card_tags_default">
-					<q-card-section>
-						<div class="row items-center no-wrap">
-							<div class="col">
-								<div class="text-h6">Tags</div>
-							</div>
 
-							<div class="col-auto">
-								<q-btn color="grey-7" round flat icon="more_vert">
-									<q-menu cover auto-close>
-										<q-list>
-											<q-item clickable>
-												<q-item-section>Link One</q-item-section>
-											</q-item>
-											<q-item clickable>
-												<q-item-section>Link Two </q-item-section>
-											</q-item>
-											<q-item clickable>
-												<q-item-section>Link Three</q-item-section>
-											</q-item>
-										</q-list>
-									</q-menu>
-								</q-btn>
+				<div class="tags_wrapper">
+					<q-card flat bordered class="card_tags_default">
+						<q-card-section>
+							<div class="row items-center no-wrap">
+								<div class="col">
+									<div class="row">
+										<q-icon name="sell" size="30px" class="q-mr-sm" />
+										<div class="text-h6">Tags</div>
+									</div>
+								</div>
+
+								<div class="col-auto">
+									<q-btn color="grey-7" round flat icon="more_vert">
+										<q-menu cover auto-close class="card_actions">
+											<q-list>
+												<q-item clickable>
+													<q-item-section>Link One</q-item-section>
+												</q-item>
+												<q-item clickable>
+													<q-item-section>Link Two </q-item-section>
+												</q-item>
+												<q-item clickable>
+													<q-item-section>Link Three</q-item-section>
+												</q-item>
+											</q-list>
+										</q-menu>
+									</q-btn>
+								</div>
 							</div>
-						</div>
-					</q-card-section>
-					<q-card-section>
-						<ul class="cards_tags_wrapper">
-							<li v-for="(tag, index) in tags" :key="index">{{ tag }}</li>
-						</ul>
-					</q-card-section>
-				</q-card>
+						</q-card-section>
+						<q-card-section>
+							<ul class="cards_tags_wrapper">
+								<li v-for="(tag, index) in tags" :key="index">{{ tag }}</li>
+							</ul>
+						</q-card-section>
+					</q-card>
+				</div>
+			</div>
+			<div class="panel_wrapper q-mt-lg">
+				<Tabs
+					:allTags="tags"
+					:teamProducts="teamProducts"
+					:teamMembers="teamMembers"
+					:teamLibraries="teamLibraries"
+					:teamEnvironnements="teamEnvironnements"
+				/>
 			</div>
 		</q-page>
 	</q-layout>
@@ -92,36 +169,12 @@
 
 <script>
 import { defineComponent, ref } from "vue";
-import getDataItems from "../composables/getDataItems";
 import AjaxBar from "../components/Progress/AjaxBar.vue";
-
-const buttonsList = [
-	{
-		title: "Add new Team",
-		styles: "bg-white q-mt-sm",
-		color: "primary",
-		icon: "add",
-		link: "/",
-	},
-	{
-		title: "Add to Favorite",
-		styles: "bg-white q-mt-sm",
-		color: "primary",
-		icon: "favorite",
-		link: "#/favorite",
-	},
-	{
-		title: "Root Team",
-		styles: "bg-white q-mt-sm",
-		color: "primary",
-		icon: "add",
-		link: "#/teams",
-	},
-];
+import Tabs from "../components/Ui/TabPanels/Tabs.vue";
 
 export default defineComponent({
 	name: "PageTeamChild",
-	components: { AjaxBar },
+	components: { AjaxBar, Tabs },
 
 	setup() {
 		const tags = ref([
@@ -131,22 +184,29 @@ export default defineComponent({
 			"Tag Four",
 			"Tag Five",
 		]);
+
+		const child = ref([
+			{
+				id: 1,
+				icon: "group",
+				headline: "Groupe SG / BDDF",
+				subTitle: "Banque de détail SG France",
+				logo: "https://cdn.quasar.dev/img/parallax2.jpg",
+				textContent:
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+			},
+		]);
 		const oepnDialog = ref(false);
-		const { path, dataItems, error, fetchData } = getDataItems();
-		const response = fetchData("http://localhost:3000/teams");
-		dataItems.value = response;
-		console.log("dataItems.value : ", dataItems);
 
 		return {
-			progress: dataItems.length,
+			// progress: tags.length,
 			lorem:
 				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			path,
-			dataItems,
-			error,
-			buttonsList,
+
 			oepnDialog,
 			tags,
+			child,
+			drawer: ref(false),
 		};
 	},
 });
@@ -157,8 +217,17 @@ export default defineComponent({
   flex-direction: row
   justify-content: space-evenly
   align-items: center
-
+.q-drawer--left
+  z-index: 1 !important
 .teams_buttons__container
   display: flex
   flex-direction: column
+.menuStyle
+  background: #eeeeee
+  border-radius: 0
+  margin-top: -41px
+.hiddenMenu
+  transform: translateX(220px)
+.visibleMenu
+  transform: translateX(-28px)
 </style>
