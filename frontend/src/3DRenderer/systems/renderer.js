84364e178db1b21957e008b1ff9eba84791c775e
Drien BRETON <drien.breton@gmail.com>
2021-11-10 11:01:12 +0100

    enable renderer antialiasing

--- frontend/src/3DRenderer/systems/renderer.js
+++ frontend/src/3DRenderer/systems/renderer.js
@@ -2,7 +2,9 @@ import { WebGLRenderer } from 'three';
 import {CSS3DRenderer} from "three/examples/jsm/renderers/CSS3DRenderer";
 
 function createRenderer() {
-	const renderer = new WebGLRenderer();
+	const renderer = new WebGLRenderer({
+		antialias: true
+	});
 
 	return renderer;
 }
