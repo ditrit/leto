1d2f835b79da144d78abca216323e41230c18284
BrahimS <bsourny@gmail.com>
2021-11-17 17:48:38 +0100

    working on the Settings page -- first part --

--- frontend/src/pages/PageSettings.vue
+++ frontend/src/pages/PageSettings.vue
@@ -1,6 +1,35 @@
 <template>
 	<q-layout class="page_padding">
 		<AjaxBar />
+		<Drawer>
+			<template v-slot:drawerFilter>
+				<div class="search_container">
+					<q-input ref="filterRef" filled v-model="filter" label="Search">
+						<template v-slot:append>
+							<q-icon
+								v-if="filter !== ''"
+								name="clear"
+								class="cursor-pointer"
+								@click="resetFilter"
+							/>
+							<q-icon
+								v-else
+								name="search"
+								class="cursor-pointer"
+								@click="resetFilter"
+							/>
+						</template>
+					</q-input>
+				</div>
+			</template>
+			<template v-slot:drawerMenu>
+				<ul>
+					<li>10</li>
+					<li>10</li>
+					<li>10</li>
+				</ul>
+			</template>
+		</Drawer>
 		<q-page class="flex flex-center bg-gray">
 			<h2>Settings Page</h2>
 		</q-page>
@@ -8,11 +37,24 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { ref, defineComponent } from "vue";
 import AjaxBar from "../components/UI/Progress/AjaxBar";
+import Drawer from "../components/UI/Drawers/Drawer";
 
 export default defineComponent({
 	name: "PageSettings",
-	components: { AjaxBar },
+	components: { AjaxBar, Drawer },
+	setup() {
+		const filter = ref("");
+		const filterRef = ref(null);
+		return {
+			filter,
+			filterRef,
+			resetFilter() {
+				filter.value = "";
+				filterRef.value.focus();
+			},
+		};
+	},
 });
 </script>
