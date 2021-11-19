134b1795b3587df5cb8a503a13cad24dc7686ffa
BrahimS <bsourny@gmail.com>
2021-11-19 16:00:19 +0100

    Merge

--- frontend/src/3DRenderer/components/Item.js
+++ frontend/src/3DRenderer/components/Item.js
@@ -10,73 +10,67 @@ import {
 	MeshStandardMaterial,
 	Group,
 	ImageLoader,
-	Color
+	Color,
 } from "three";
-import {Grid} from "src/3DRenderer/systems/Grid";
-import {CSS3DObject} from 'three/examples/jsm/renderers/CSS3DRenderer'
+import { Grid } from "src/3DRenderer/systems/Grid";
+import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 
 class Item {
 	constructor(params) {
-		this.height = 0.4
+		this.height = 0.4;
 		//this.width = 1
 		//this.depth = 1
-		this.baseColor = params.color
-		delete params.color
-		Object.assign(this, params)
-		this.links = []
+		this.baseColor = params.color;
+		delete params.color;
+		Object.assign(this, params);
+		this.links = [];
 
-		this.grid = new Grid(/*this.items.filter(i => i.parentId === item.id)*/ [], this, true)
-		this.grid.gridSpacing = 1
-		this.img = null
-		this.sideCanvas = document.createElement('canvas');
-		this.topCanvas = document.createElement('canvas');
+		this.grid = new Grid(
+			/*this.items.filter(i => i.parentId === item.id)*/ [],
+			this,
+			true
+		);
+		this.grid.gridSpacing = 1;
+		this.img = null;
+		this.sideCanvas = document.createElement("canvas");
+		this.topCanvas = document.createElement("canvas");
 
-		const loader = new ImageLoader()
+		const loader = new ImageLoader();
 		loader.load(this.logo, (image) => {
-			this.img = image
-			this.updateTexture()
-		})
-
+			this.img = image;
+			this.updateTexture();
+		});
 	}
 
 	get color() {
+		if (this.nature === "service") return new Color("#2cab4c");
+		if (this.nature === "concrete") return new Color("#00359f");
+		if (this.nature === "abstract") return new Color("#3a5b9d");
 
-		if (this.nature === 'service')
-			return new Color('#2cab4c')
-		if (this.nature === 'concrete')
-			return new Color('#00359f')
-		if (this.nature === 'abstract')
-			return new Color('#3a5b9d')
-
-		return new Color(this.baseColor)
+		return new Color(this.baseColor);
 	}
 	get colorHex() {
-		return `#${this.color.getHexString()}`
+		return `#${this.color.getHexString()}`;
 	}
-
 
 	get width() {
-		return this.grid.width
+		return this.grid.width;
 	}
 	get depth() {
-		return this.grid.depth
+		return this.grid.depth;
 	}
 	updateCanvas() {
 		const width = this.width * 500;
-		const cellWidth = (this.grid.baseWidth + this.grid.gridSpacing) * 500
-		const cellDepth = cellWidth / 3
+		const cellWidth = (this.grid.baseWidth + this.grid.gridSpacing) * 500;
+		const cellDepth = cellWidth / 3;
 		const height = this.height * 500;
-		const depth = this.depth * 500
-		this.topCanvas.width = cellWidth
-		this.topCanvas.height = this.topCanvas.width / 3
-		this.sideCanvas.width  = width;
+		const depth = this.depth * 500;
+		this.topCanvas.width = cellWidth;
+		this.topCanvas.height = this.topCanvas.width / 3;
+		this.sideCanvas.width = width;
 		this.sideCanvas.height = height;
-		console.log('item color', this.color, this.colorHex)
-		console.log('item name', this.name)
-
-
-
-
+		console.log("item color", this.color, this.colorHex);
+		console.log("item name", this.name);
 
 		// use the image, e.g. draw part of it on a sideCanvas
 		let ctx = this.sideCanvas.getContext("2d");
@@ -99,14 +93,12 @@ class Item {
 		ctx.fill();
 		ctx.closePath()*/
 
-
-
 		const tOrigin = {
 			x: cellWidth / 3,
-			y: 0
-		}
+			y: 0,
+		};
 
-		const tctx = this.topCanvas.getContext('2d')
+		const tctx = this.topCanvas.getContext("2d");
 
 		// Background
 
@@ -116,12 +108,10 @@ class Item {
 		tctx.fill();
 		tctx.closePath();
 
-
-
 		tctx.shadowOffsetX = 5;
 		tctx.shadowOffsetY = 5;
 		tctx.shadowBlur = 2;
-		tctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
+		tctx.shadowColor = "rgba(0, 0, 0, 0.5)";
 		/*tctx.beginPath()
 		tctx.rect(0, 0, cellWidth, depth);
 		tctx.fillStyle = "rgba(0, 0, 0, .1)";
@@ -131,38 +121,25 @@ class Item {
 		tctx.fill();
 		tctx.closePath()*/
 		tctx.beginPath();
-		tctx.rect(0, 0, this.topCanvas.width-2,
-		this.topCanvas.height -2);
+		tctx.rect(0, 0, this.topCanvas.width - 2, this.topCanvas.height - 2);
 		tctx.fillStyle = this.colorHex;
 		tctx.fill();
 		tctx.fillStyle = "rgba(0, 0, 0, .1)";
-		tctx.stroke()
+		tctx.stroke();
 		tctx.closePath();
 
-
-		tctx.textAlign = 'left';
-		tctx.fillStyle = '#FFFFFF';
-		tctx.font = 'bold 150px Helvetica';
-		tctx.fillText(this.type, 30,cellWidth/4 + 125 ,   cellWidth / 4);
+		tctx.textAlign = "left";
+		tctx.fillStyle = "#FFFFFF";
+		tctx.font = "bold 150px Helvetica";
+		tctx.fillText(this.type, 30, cellWidth / 4 + 125, cellWidth / 4);
 		//tctx.font = '90pt Calibri';
-		tctx.font = '150px Helvetica';
-		tctx.fillText(this.name, tOrigin.x + 50, 350, 2 * cellWidth / 3);
-
-
+		tctx.font = "150px Helvetica";
+		tctx.fillText(this.name, tOrigin.x + 50, 350, (2 * cellWidth) / 3);
 
 		if (this.img) {
 			//	ctx.drawImage(this.img, 0,0, height, height);
-			tctx.drawImage(this.img, 0,0, cellWidth / 4 , cellWidth / 4);
+			tctx.drawImage(this.img, 0, 0, cellWidth / 4, cellWidth / 4);
 		}
-
-
-
-
-
-
-
-
-
 
 		/*return new Promise((resolve) => {
 			if (!this.logo) return resolve()
@@ -174,34 +151,31 @@ class Item {
 				resolve()
 			}, false);
 		})*/
-
 	}
 	generateTexture(canvas) {
-
 		const texture = new CanvasTexture(canvas);
 
-		return texture
-
+		return texture;
 	}
 	generateMaterial() {
 		//const material = [
-			//new MeshStandardMaterial({ color: this.color/*map: this.sideTexture*//*color: this.color*/ }),	// Right side
+		//new MeshStandardMaterial({ color: this.color/*map: this.sideTexture*//*color: this.color*/ }),	// Right side
 		//	new MeshStandardMaterial({ color: this.color/*map: this.sideTexture*//*color: this.color*/ }),	// Left side
 		//	new MeshStandardMaterial({color: this.color/*map: this.sideTexture*/}),	// Top side
 		//	new MeshStandardMaterial({color: this.color/*map: this.sideTexture */}),	// Bottom side
 		//	new MeshStandardMaterial({color: this.color/*map: this.sideTexture*/}),	// Front side
 		//	new MeshStandardMaterial({color: this.color/*map: this.sideTexture*/})	// Back side
-	//	];*/
+		//	];*/
 
 		const vertexShader = `
     varying vec2 vUv;
-    uniform vec3 uColor;
+    // uniform vec3 uColor;
     void main()	{
       vUv = uv;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
     }
   `;
-		console.log('item color', this.color, this.color.toArray())
+		console.log("item color", this.color, this.color.toArray());
 		const fragmentShader = `
 		//#extension GL_OES_standard_derivatives : enable
 		precision mediump float;
@@ -223,32 +197,33 @@ class Item {
       gl_FragColor = vec4(c, 1.0);
     }
   `;
-	const material = new ShaderMaterial({
+		const material = new ShaderMaterial({
 			uniforms: {
-				uColor: {type: 'c', value: this.color},
+				uColor: { type: "c", value: this.color },
 				thickness: {
-					value: 1.5
-				}
+					value: 1.5,
+				},
 			},
 			vertexShader: vertexShader,
-			fragmentShader: fragmentShader
+			fragmentShader: fragmentShader,
 		});
-		return material
+		return material;
 	}
 	generateComponent(material) {
-
 		//const finalObject = new Group()
-		const newComponent = new Mesh(new BoxGeometry(this.width, this.height, this.depth), material)
+		const newComponent = new Mesh(
+			new BoxGeometry(this.width, this.height, this.depth),
+			material
+		);
 
 		//finalObject.add(newComponent)
 
 		//return finalObject
-		return newComponent
-
+		return newComponent;
 	}
 	isParentOfUUID(uuid) {
-		if(this.threeObj.uuid === uuid) return true
-		return  this.grid.items.reduce((a, i) => a || i.isParentOfUUID(uuid), false)
+		if (this.threeObj.uuid === uuid) return true;
+		return this.grid.items.reduce((a, i) => a || i.isParentOfUUID(uuid), false);
 	}
 
 	/*get width() {
@@ -264,9 +239,13 @@ class Item {
 		}*/
 		/*this.width = newWidth
 		this.depth = newDepth*/
-		this.threeObj.geometry.dispose()
-		this.threeObj.geometry = new BoxGeometry(this.width, this.height, this.depth)
-		this.updateTexture()
+		this.threeObj.geometry.dispose();
+		this.threeObj.geometry = new BoxGeometry(
+			this.width,
+			this.height,
+			this.depth
+		);
+		this.updateTexture();
 
 		//this.grid.updatePlacement()
 		/*const newObj = await this.create3DItem()
@@ -275,11 +254,10 @@ class Item {
 		this.threeObj.removeFromParent()
 		this.threeObj = newObj
 		scene.add(this.threeObj)*/
-
 	}
 	update(newData) {
-		Object.assign(this, newData)
-		console.log('sideCanvas updated')
+		Object.assign(this, newData);
+		console.log("sideCanvas updated");
 		if (this.isSelected) {
 			//this.threeObj.material.forEach(m =>m.emissive.setHex( 0xff0000 ))
 			//	this.sprite.material.emissive.setHex( 0xff0000 )
@@ -287,49 +265,52 @@ class Item {
 			//this.threeObj.material.forEach(m =>m.emissive.setHex( 0x000000 ))
 			//this.sprite.material.emissive.setHex( 0x000000 )
 		}
-		this.updateTexture()
+		this.updateTexture();
 		//this.threeObj.material
 	}
 	updateTexture() {
-		this.updateCanvas()
-		this.sideTexture.needsUpdate = true
-		this.topTexture.needsUpdate = true
+		this.updateCanvas();
+		this.sideTexture.needsUpdate = true;
+		this.topTexture.needsUpdate = true;
 	}
 	createInfoCard() {
-		const element = document.createElement( 'div' );
-		element.className = 'element';
-		element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
-		this.infoCard = new CSS3DObject(element)
-		this.infoCard.position.x = 0
-		this.infoCard.position.y = 0
-		this.infoCard.position.z = 0
-		return this.infoCard
+		const element = document.createElement("div");
+		element.className = "element";
+		element.style.backgroundColor =
+			"rgba(0,127,127," + (Math.random() * 0.5 + 0.25) + ")";
+		this.infoCard = new CSS3DObject(element);
+		this.infoCard.position.x = 0;
+		this.infoCard.position.y = 0;
+		this.infoCard.position.z = 0;
+		return this.infoCard;
 	}
 	createSprite() {
-		const material = new SpriteMaterial({map: this.topTexture})
-		return new Sprite(material)
+		const material = new SpriteMaterial({ map: this.topTexture });
+		return new Sprite(material);
 	}
 	updateSpritePosition() {
-		this.sprite.position.y = this.threeObj.position.y + (this.grid.baseWidth / 3)
-		this.sprite.position.z = this.threeObj.position.z
-		this.sprite.position.x = this.threeObj.position.x - (this.width / 2) + (this.grid.baseWidth + this.grid.gridSpacing) /2
+		this.sprite.position.y = this.threeObj.position.y + this.grid.baseWidth / 3;
+		this.sprite.position.z = this.threeObj.position.z;
+		this.sprite.position.x =
+			this.threeObj.position.x -
+			this.width / 2 +
+			(this.grid.baseWidth + this.grid.gridSpacing) / 2;
 	}
 	async create3DItem() {
-		this.updateCanvas()
-		this.sideTexture = this.generateTexture(this.sideCanvas)
-		this.topTexture = this.generateTexture(this.topCanvas)
+		this.updateCanvas();
+		this.sideTexture = this.generateTexture(this.sideCanvas);
+		this.topTexture = this.generateTexture(this.topCanvas);
 		const material = this.generateMaterial();
-		this.sprite = this.createSprite()
-		this.sprite.scale.set(this.grid.baseWidth, this.grid.baseWidth / 3, 1)
-		this.threeObj =  this.generateComponent(material)
-		this.threeObj.castShadow = true
-		this.threeObj.recieveShadow = true
+		this.sprite = this.createSprite();
+		this.sprite.scale.set(this.grid.baseWidth, this.grid.baseWidth / 3, 1);
+		this.threeObj = this.generateComponent(material);
+		this.threeObj.castShadow = true;
+		this.threeObj.recieveShadow = true;
 
-		this.updateSpritePosition()
+		this.updateSpritePosition();
 
-		return this.threeObj
-
+		return this.threeObj;
 	}
 }
 
-export {Item}
+export { Item };
