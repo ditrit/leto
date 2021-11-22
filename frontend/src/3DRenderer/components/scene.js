7905bd84017e5c93e275ff93b70c45ce998a933d
Drien BRETON <drien.breton@gmail.com>
2021-11-22 16:31:08 +0100

    update sprite generation

--- frontend/src/3DRenderer/components/scene.js
+++ frontend/src/3DRenderer/components/scene.js
@@ -3,7 +3,7 @@ import { Color, Scene, AmbientLight, PointLight } from 'three';
 function createScene() {
 	const scene = new Scene();
 
-	scene.background = new Color("#bababa");
+	scene.background = new Color("#eaeaea");
 
 	return scene;
 }
