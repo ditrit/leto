b75300e80e5f67f3f875f2311923ff5514b0c137
BrahimS <bsourny@gmail.com>
2022-02-09 10:41:49 +0100

    Refresh authorization data after Update Event

--- frontend/src/components/UI/Dialogs/Modal.vue
+++ frontend/src/components/UI/Dialogs/Modal.vue
@@ -12,12 +12,14 @@
 				<q-dialog v-model="isOpened" v-show="oepnDialog" position="bottom">
 					<q-card style="width: 750px; max-width: 80vw">
 						<q-card-section>
-							<q-card-actions
-								align="right"
-								position="top"
-								class="bg-white text-teal"
-							>
-								<q-btn round color="primary" icon="close" v-close-popup />
+							<q-card-actions align="right" class="bg-white text-teal">
+								<q-btn
+									outline
+									round
+									color="primary"
+									icon="close"
+									v-close-popup
+								/>
 							</q-card-actions>
 							<div class="text-h6 q-pa-md">
 								<slot name="ModalHeadline"> </slot>
