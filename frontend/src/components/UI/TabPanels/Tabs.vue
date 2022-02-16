727fcb2b4e1270cd87b8d4baa2a1855c0e8bd0d2
BrahimS <bsourny@gmail.com>
2022-02-16 17:11:16 +0100

    Create a modal to add new Library

--- frontend/src/components/UI/TabPanels/Tabs.vue
+++ frontend/src/components/UI/TabPanels/Tabs.vue
@@ -322,9 +322,108 @@
 							text-color="primary"
 							icon="add"
 							label="New library"
-							@click.prevent="openCreationModal(teamLibraries)"
+							@click.prevent="openCreationLibraryModal(teamLibraries)"
 						/>
 					</div>
+					<!-- Products Creation dialog -->
+					<q-dialog v-model="isCreationLibraryOpened" persistent>
+						<q-card style="width: 750px; max-width: 80vw">
+							<q-card-section>
+								<div class="text-h6 q-pa-md">{{ $t("add_library") }}</div>
+							</q-card-section>
+
+							<q-card-section class="q-pt-none">
+								<q-form
+									@submit.prevent="addNewProduct"
+									@reset="onResetProduct"
+									class="q-gutter-sm q-pa-md"
+								>
+									<div class="row col-md-12 q-gutter-md">
+										<div class="col">
+											<q-input
+												filled
+												label="Name *"
+												hint=""
+												lazy-rules
+												:rules="[
+													(val) =>
+														(val && val.length > 0) || 'Please type something',
+												]"
+												v-model="productName"
+											/>
+										</div>
+									</div>
+									<q-input
+										class="q-gutter-md"
+										filled
+										label="Short Description *"
+										lazy-rules
+										:rules="[
+											(val) =>
+												(val && val.length > 0) || 'Please type something',
+										]"
+										v-model="productShortDescription"
+									/>
+									<q-input
+										class="q-gutter-md"
+										filled
+										label="Repo *"
+										lazy-rules
+										:rules="[
+											(val) =>
+												(val && val.length > 0) || 'Please type something',
+										]"
+										v-model="productProductRepositoryURL"
+									/>
+
+									<div class="row q-gutter-md">
+										<div class="col col-md-8">
+											<q-input
+												class="q-gutter-md"
+												filled
+												type="textarea"
+												label="Description *"
+												lazy-rules
+												:rules="[
+													(val) =>
+														(val && val.length > 0) || 'Please type something',
+												]"
+												v-model="productDescription"
+											/>
+										</div>
+										<div class="col">
+											<q-uploader
+												style="max-width: 100%"
+												url="http://localhost:3000/upload"
+												label="Your Logo"
+												multiple
+												accept=".jpg, svg, image/*"
+												@rejected="onRejected"
+												color="primary"
+												factory
+												files
+												hide-upload-btn="true"
+												auto-upload
+												@uploaded="onFileUpload"
+											/>
+										</div>
+									</div>
+									<q-card-actions
+										align="right"
+										class="text-primary flex justify-center"
+									>
+										<q-btn type="reset" label="Cancel" v-close-popup />
+										<q-btn
+											label="Create"
+											type="submit"
+											color="primary"
+											v-close-popup
+										/>
+									</q-card-actions>
+								</q-form>
+							</q-card-section>
+						</q-card>
+					</q-dialog>
 				</q-tab-panel>
 
 				<q-tab-panel name="environnements" class="flex q-gutter-md">
@@ -659,6 +758,7 @@ export default {
 		const domainID = ref(route.currentRoute.value.params.id);
 		const isAuthorCreationOpened = ref(false);
 		const isCreationProductsOpened = ref(false);
+		const isCreationLibraryOpened = ref(false);
 		const isModificationProductsOpened = ref(false);
 		const isEnvironmentsModificationOpened = ref(false);
 		const authorizationDomainNameRef = ref(null);
@@ -675,6 +775,10 @@ export default {
 
 		const openCreationProductModal = (props) => {
 			isCreationProductsOpened.value = true;
+			emit("openNewItemModal", props);
+		};
+		const openCreationLibraryModal = (props) => {
+			isCreationLibraryOpened.value = true;
 			emit("openNewItemModal", props);
 		};
 
@@ -804,6 +908,8 @@ export default {
 			onSubmitAuthorization,
 			domainList,
 			confirmDeleteAuthorization,
+			openCreationLibraryModal,
+			isCreationLibraryOpened,
 			model: ref(null),
 			options: ["BDDF", "GIMS", "SGCIB", "BHUFM", "GTS"],
 		};
