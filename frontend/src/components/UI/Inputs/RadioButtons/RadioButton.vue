40a87330bb9a7284385bfda5023545a2342b085e
brahims <bsourny@gmail.com>
2022-03-07 14:41:14 +0100

    Create a radio button input component

---
+++ frontend/src/components/UI/Inputs/RadioButtons/RadioButton.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<div class="row q-pa-sm">
+			<div class="col-4 q-pt-sm">
+				<slot name="checkBoxTitle"> Title </slot>
+			</div>
+
+			<div class="col-8">
+				<q-checkbox v-model="checkValue" :label="labelValue" />
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+	props: {
+		labelV: {
+			type: String,
+			default: "Repository",
+		},
+	},
+	setup(props) {
+		const textValue = ref(props.textV);
+		const labelValue = ref(props.labelV);
+		const checkValue = ref(false);
+		return {
+			textValue,
+			labelValue,
+			checkValue,
+		};
+	},
+};
+</script>
