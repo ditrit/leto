9d5c4218995f2617f6b8076b92d7e087f4a97031
brahims <bsourny@gmail.com>
2022-04-21 11:57:37 +0200

    Fix issues after sonar review

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -3,43 +3,6 @@ import axios from "axios";
 const API = axios.create({
 	baseURL: "http://127.0.0.1:9203/ditrit/Gandalf/1.0.0",
 });
-// const userString = localStorage.getItem("user");
-// const userData = JSON.parse(userString);
-
-// API.interceptors.request.use(
-// 	(config) => {
-// 		let params = new URLSearchParams();
-// 		// console.log("userData requiste : ", userData);
-// 		axios.defaults.headers.common[
-// 			"Authorization"
-// 		] = `Bearer ${userData.accessToken}`;
-// 		config.headers.Authorization = `Bearer ${userData.accessToken}`;
-// 		params.append("Authorization", `Bearer ${userData.accessToken}`);
-// 		config.params = params;
-// 		return config;
-// 	},
-// 	function (error) {
-// 		return Promise.reject(error);
-// 	}
-// );
-
-// API.interceptors.response.use(
-// 	(config) => {
-// 		let params = new URLSearchParams();
-
-// 		// console.log("userData requiste : ", userData);
-// 		axios.defaults.headers.common[
-// 			"Authorization"
-// 		] = `Bearer ${userData.accessToken}`;
-// 		config.headers.Authorization = `Bearer ${userData.accessToken}`;
-// 		params.append("Authorization", `Bearer ${userData.accessToken}`);
-// 		config.params = params;
-// 		return config;
-// 	},
-// 	function (error) {
-// 		return Promise.reject(error);
-// 	}
-// );
 
 if (localStorage.getItem("user")) {
 	API.interceptors.request.use(
