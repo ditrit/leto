1f9023ef92c7ecaf1e3d7322d6a356515374bc93
brahims <bsourny@gmail.com>
2022-04-21 17:31:42 +0200

    Install tipTap

--- frontend/src/components/Builder/Grapes.vue
+++ frontend/src/components/Builder/Grapes.vue
@@ -5,30 +5,33 @@
 <script>
 import { onMounted } from "vue";
 import grapesjs from "grapesjs";
+import tipTap from "@tiptap/core";
+import tipTapStarter from "@tiptap/starter-kit";
 import "grapesjs/dist/css/grapes.min.css";
 import "grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css";
 import "grapesjs-preset-webpage";
+console.log("tipTap: ", tipTap);
 
 export default {
 	name: "Grapes",
 	setup() {
-		console.log("grapesjs: ", grapesjs);
-
 		onMounted(() => {
-			grapesjs.init({
+			const editor = grapesjs.init({
 				container: "#gjs",
 				fromElement: true,
-				height: "800px",
 				width: "1100px",
 				storageManager: { autoload: 0 },
 				showOffsets: 1,
 				panels: { defaults: [] },
-				plugins: ["gjs-preset-webpage"],
+				plugins: ["gjs-preset-webpage", "@tiptap/core", "@tiptap/starter-kit"],
 				pluginsOpts: { "gjs-preset-webpage": {} },
 			});
+			console.log("editor: ", editor.editor.attributes.RichTextEditor);
+
+			return editor;
 		});
 
-		return {};
+		return { tipTap, tipTapStarter };
 	},
 };
 </script>
--- frontend/src/store/Users/mutations.js
+++ frontend/src/store/Users/mutations.js
@@ -1,8 +1,10 @@
 export const GET_USERS = (state, theUsers) => (state.theUsers = theUsers);
 export const NEW_USER = (state, user) => state.theUsers.unshift(user);
+
 export const UPDATE_USER = (state, updatedUser) => {
-	const index = state.theUsers.findIndex((user) => user.id === updatedUser);
-	state.theUsers.splice(index, 1, updatedUser);
+	console.log("updatedUser: ", updatedUser);
+	const index = state.theUsers.findIndex((user) => user.id === updatedUser.ID);
+	state.theUsers[index] = updatedUser;
 };
 export const DELETE_USER = (state, userId) =>
 	(state.theUsers = state.theUsers.filter((user) => user.id !== userId));
