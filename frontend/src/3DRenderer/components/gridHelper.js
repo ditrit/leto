0a6517602a217f94ad7fe6f3d622e964f9b4a062
brahims <bsourny@gmail.com>
2022-04-25 13:48:58 +0200

    Removed the 3DRenderer + 3dModels folders

--- frontend/src/3DRenderer/components/gridHelper.js
+++
@@ -1,9 +0,0 @@
-import {GridHelper} from "three";
-
-function createGridHelper() {
-	const gridHelper = new GridHelper(200, 200)
-	gridHelper.position.y = -0.5
-	return gridHelper
-}
-
-export {createGridHelper}
