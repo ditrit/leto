d2f77cb1f3eb01e7b8e484e26d8e5997b0dbf174
brahims <bsourny@gmail.com>
2022-04-29 16:13:50 +0200

    Add First reusable block

--- frontend/src/components/Builder/Grapes.vue
+++ frontend/src/components/Builder/Grapes.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		<section id="gjs"></section>
-		<section id="block"></section>
 	</div>
 </template>
 
@@ -32,17 +31,14 @@ export default {
 			});
 			editor.value.BlockManager.add("#block", {
 				content: {
-					tagName: "div",
+					tagName: "section",
 					draggable: true,
 					attributes: { class: "block_1" },
 					components: [
 						{
-							tagName: "span",
-							content: "<b>Some static content</b>",
-						},
-						{
-							tagName: "div",
-							components: "<span>HTML at some point</span>",
+							tagName: "article",
+							content:
+								"<h2>This is a title</h2> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusda recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur ugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!</p>",
 						},
 					],
 				},
@@ -61,8 +57,6 @@ export default {
 
 <style lang="sass">
 #gjs
-
-
 
 .gjs-dashed body
   background: #e0e0e0 !important
