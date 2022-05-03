8ecf1d5bdebf8c7a2b305eb22d4e6d56d1110283
brahims <bsourny@gmail.com>
2022-05-03 09:10:59 +0200

    Refactor AvatarImg component

--- frontend/src/components/UI/Tables/UsersTable.vue
+++ frontend/src/components/UI/Tables/UsersTable.vue
@@ -21,13 +21,11 @@
 			table-header-class="table_header"
 		>
 			<template v-slot:body-cell-avatar="props">
-				<q-td :props="props">
-					<AvatarImg
-						:source="props.row.avatar"
-						:substitute="globalAvatar"
-						alt="user avatar"
-					/>
-				</q-td>
+				<AvatarImg
+					:source="props.row.avatar"
+					:substitute="globalAvatar"
+					alt="user avatar"
+				/>
 			</template>
 			<template v-slot:body-cell-actionsButtons="props">
 				<q-td :props="props">
