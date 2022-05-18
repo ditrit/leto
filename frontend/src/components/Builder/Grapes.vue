25661f143a91ca7f2553901c8fa2fdb6b75e767d
brahims <bsourny@gmail.com>
2022-05-18 15:22:28 +0200

    Remove dead code

--- frontend/src/components/Builder/Grapes.vue
+++ frontend/src/components/Builder/Grapes.vue
@@ -7,7 +7,6 @@
 <script>
 import { ref, onMounted } from "vue";
 import grapesjs from "grapesjs";
-import "ckeditor4";
 import blocks from "grapesjs-blocks-basic";
 import "grapesjs/dist/css/grapes.min.css";
 
@@ -49,15 +48,4 @@ export default {
 
 		return { editor, initGrapes };
 	},
-};
-</script>
-
-<style lang="sass">
-#gjs
-
-.gjs-dashed body
-  background: #e0e0e0 !important
-
-.gjs-editor-cont
-  margin: 45px 0 45px 45px
-</style>
+	}
