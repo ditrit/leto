49a8f7ab394dbee5718ab31d6616289640ae06d4
brahims <bsourny@gmail.com>
2022-05-20 10:36:39 +0200

    Install missing packages

--- frontend/src/components/Monaco/Monaco.vue
+++ frontend/src/components/Monaco/Monaco.vue
@@ -4,74 +4,74 @@
 		<button :onClick="consoleClick" class="Button">
 			<slot>Button</slot>
 		</button>
-	</div>	
+	</div>
 </template>
 
 <script>
 import * as monaco from "monaco-editor";
-import {hclTokensProvider} from './hclTokensProvider.js';
-import Button from '../UI/Buttons/BtnAddNew.vue'
+import { hclTokensProvider } from "./hclTokensProvider.js";
+import Button from "../UI/Buttons/BtnAddNew.vue";
 
 export default {
 	data() {
 		return {
 			monacoEditor: {},
-			iactorDatas : {},			
-    		'button': Button
+			iactorDatas: {},
+			button: Button,
 		};
 	},
-	mounted() {	
+	mounted() {
 		this.worker = new Worker("dist/bundle.js");
 		this.worker.onmessage = function (event) {
 			console.log("message post in the worker");
-			console.log(event.data);
-			this.iactorDatas = event.data
-		}
+			this.iactorDatas = event.data;
+			console.log("this.iactorDatas: ", this.iactorDatas);
+		};
 		// Initialize the editor, make sure the dom has been rendered, and the dialog should be written in opened
-		monaco.languages.register({ id: 'hcl' });
-		monaco.languages.setTokensProvider('hcl', hclTokensProvider.prototype);
-		let string = 'BF2937';
-		let identifier = '131DB4';
-		let symbolsFg = '000000';
-		let object = '4FA786';
-		let errorFg = 'ff0000';
-		let comment = '07812C';
-		let boolean = '105FEE';
-		this.valueEditor = '';
+		monaco.languages.register({ id: "hcl" });
+		monaco.languages.setTokensProvider("hcl", hclTokensProvider.prototype);
+		let string = "BF2937";
+		let identifier = "131DB4";
+		let symbolsFg = "000000";
+		let object = "4FA786";
+		let errorFg = "ff0000";
+		let comment = "07812C";
+		let boolean = "105FEE";
+		this.valueEditor = "";
 
-		monaco.editor.defineTheme('terraformTheme', {
-			base: 'vs',
+		monaco.editor.defineTheme("terraformTheme", {
+			base: "vs",
 			inherit: false,
 			rules: [
-				{ token: 'output.hcl',   foreground: object },
-				{ token: 'variable.hcl',   foreground: object },
-				{ token: 'module.hcl',   foreground: object },
-				{ token: 'terraform.hcl',   foreground: object },
-				{ token: 'provider.hcl',   foreground: object },
-				{ token: 'resource.hcl',   foreground: object },
-				{ token: 'string.hcl',   foreground: string },
-				{ token: 'type.hcl',           foreground: symbolsFg },
-				{ token: 'boolean.hcl',       foreground: boolean,  fontStyle: 'bold' },
-				{ token: 'booleanop.hcl',       foreground: symbolsFg },
-				{ token: 'index.hcl',        foreground: symbolsFg },
-				{ token: 'identifier.hcl',          foreground: identifier },
-				{ token: 'number.hcl',          foreground: symbolsFg },
-				{ token: 'comment.hcl',     foreground: comment,  fontStyle: 'bold' },
-				{ token: 'line_comment.hcl',    foreground: comment,  fontStyle: 'bold' },
-				{ token: 'has_comment.hcl',    foreground: comment,  fontStyle: 'bold' },
-				{ token: 'error.hcl', foreground: errorFg }
+				{ token: "output.hcl", foreground: object },
+				{ token: "variable.hcl", foreground: object },
+				{ token: "module.hcl", foreground: object },
+				{ token: "terraform.hcl", foreground: object },
+				{ token: "provider.hcl", foreground: object },
+				{ token: "resource.hcl", foreground: object },
+				{ token: "string.hcl", foreground: string },
+				{ token: "type.hcl", foreground: symbolsFg },
+				{ token: "boolean.hcl", foreground: boolean, fontStyle: "bold" },
+				{ token: "booleanop.hcl", foreground: symbolsFg },
+				{ token: "index.hcl", foreground: symbolsFg },
+				{ token: "identifier.hcl", foreground: identifier },
+				{ token: "number.hcl", foreground: symbolsFg },
+				{ token: "comment.hcl", foreground: comment, fontStyle: "bold" },
+				{ token: "line_comment.hcl", foreground: comment, fontStyle: "bold" },
+				{ token: "has_comment.hcl", foreground: comment, fontStyle: "bold" },
+				{ token: "error.hcl", foreground: errorFg },
 			],
-			colors :{
-				'editor.foreground': '#000000'
-			}
-		}); 
-		let editor = monaco.editor.create(this.$refs.monacoContainer, {         
-			language: 'hcl',
-			theme: 'terraformTheme'		
+			colors: {
+				"editor.foreground": "#000000",
+			},
 		});
-		this.monacoEditor= editor
+		let editor = monaco.editor.create(this.$refs.monacoContainer, {
+			language: "hcl",
+			theme: "terraformTheme",
+		});
+		this.monacoEditor = editor;
 		this.monacoEditor.getModel().onDidChangeContent((event) => {
-			this.onChange(editor.getValue())
+			this.onChange(editor.getValue());
 		});
 	},
 	methods: {
@@ -79,14 +79,14 @@ export default {
 			this.worker.postMessage(this.valueEditor);
 		},
 		onChange(value) {
-			this.valueEditor = value
+			this.valueEditor = value;
 		},
 		sendDatas(value) {
-			console.log(value)
-			if(value != null) {
-				this.$emit('parse_datas', value)
-			}			
-		}
+			console.log(value);
+			if (value != null) {
+				this.$emit("parse_datas", value);
+			}
+		},
 	},
 };
 </script>
