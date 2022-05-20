f285ffd6b67c22f9695ec206c4ef18dbf1f0ac91
Alexis <alexis.nvu@gmail.com>
2022-05-20 19:02:15 +0200

    LetoTypeNode and ToscaTypeNode classes added

---
+++ frontend/src/components/2DModel/LetoTypeNode.js
@@ -0,0 +1,24 @@
+import SVGinstanciate from "./svgvar";
+const d3 = require("d3");
+
+export default class LetoTypeNode{
+	constructor(logo_path, type_name, primary_color, secondary_color,svg){
+		this.svg = svg;
+		this.logo_path=logo_path;
+		this.type_name=type_name;
+		this.primary_color=primary_color;
+		this.secondary_color=secondary_color;
+	}
+
+	defaultPanel={}
+
+	drawInPanel(x,y,parent,svgs){
+
+		const svgDom = SVGinstanciate(svgs[this.defaultPanel.svg], this.defaultPanel);
+		let newSvg = d3.select(parent).node().append(svgDom.documentElement);
+		let model = document.getElementById(this.defaultPanel.id);
+
+		model.setAttribute("x",x);
+		model.setAttribute("y",y);
+	}
+}
