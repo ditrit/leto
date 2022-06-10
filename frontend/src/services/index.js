c1bdfe3132ffb6a614be3ed7a6e4f8d16cf076de
brahims <bsourny@gmail.com>
2022-06-10 18:01:58 +0200

    Handle expired token

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import dayJS from "dayjs";
+
 
 const API = axios.create({
 	baseURL: process.env.BASEURL,
@@ -6,10 +9,21 @@ const API = axios.create({
 
 if (localStorage.getItem("user")) {
 	const { accessToken } = JSON.parse(localStorage.getItem("user"));
+	const user = jwtDecode(accessToken);
+	const userID = jwtDecode(accessToken).UserID;
+	const isExpired = dayJS.unix(user.exp).diff(dayJS()) < 1;
+
 	API.interceptors.request.use(
 		async (config) => {
 			config.headers.Authorization = `Bearer ${accessToken}`;
-			return config;
+			if(!isExpired) {
+				return config;
+			} else  {
+				const response = await axios.post(`${process.env.BASEURL}/user/refreshtoken/${userID}`)
+				localStorage.setItem("newToken", JSON.stringify(response.data.accessToken))
+				config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+				return config;
+			}
 		},
 		(error) => {
 			return Promise.reject(error);
@@ -19,9 +33,16 @@ if (localStorage.getItem("user")) {
 	API.interceptors.response.use(
 		async (config) => {
 			config.headers.Authorization = `Bearer ${accessToken}`;
+		if (!isExpired) {
+		return config;
+		} else {
+			config.headers.Authorization = `Bearer ${localStorage.getItem(
+				"newToken"
+			)}`;
 			return config;
+		}
 		},
-		(error) => {
+		 (error) => {
 			return Promise.reject(error);
 		}
 	);
