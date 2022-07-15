2b0374c46ba86266ae1ab3f795e0877538c9e73a
Xavier Talon <xavier.talon@orness.com>
2022-07-15 23:17:36 +0200

    Fix: Duplication of links when rewriting resources as strings

--- frontend/src/components/Monaco/toStringFunctions.js
+++ frontend/src/components/Monaco/toStringFunctions.js
@@ -1,8 +1,8 @@
 export function toStringResource(resource, type, container) {
 	let resourceToString = `${type} "${resource.type}" "${resource.name}" {\n`;
 	resourceToString += (resource.objects) ? toStringObjects(resource.objects.value, resource) : '';
-	resourceToString += (resource.links) ? toStringLinks(resource.links.outputs, true) : '';
-	resourceToString += (resource.links) ?toStringLinks(resource.links.inputs, false) : '';
+	resourceToString += (resource.links) ? toStringLinks(resource.links.outputs, true, resource.objects.value) : '';
+	resourceToString += (resource.links) ? toStringLinks(resource.links.inputs, false, resource.objects.value) : '';
     if(container !== undefined && container.length > 0) resourceToString += toStringContainer(resource, container);
 	resourceToString += `}\n`;
 	return resourceToString;
@@ -17,10 +17,10 @@ export function toString(object, type) {
 	return objectToString;
 }
 
-function toStringObjects(objects, resource) {	
+function toStringObjects(objects, resource) {
 	let res = '';
 	objects.forEach( object => {
-		if(typeof(object) === 'object') {	
+		if(typeof(object) === 'object') {
 			res += `  ${object.name} {\n`;
 			object.objects.forEach((o) => {
 				res += `    ${o}\n`;
@@ -33,41 +33,53 @@ function toStringObjects(objects, resource) {
                     find = true;
                 }
             })
-            if(!find) {                
+            if(!find) {
                 res += `  ${object}\n`;
             }
         } else {
             res += `  ${object}\n`;
-        }							
+        }
 	})
+	console.log("objects : ", res )
 	return res;
 }
 
-function toStringLinks(links, output) {	
+function toStringLinks(links, output, objects) {
 	let res = '';
     let variableNames = [];
 
 	links.forEach( link => {
-		if(output && link.representation === 'link') { 
-            if(link.multiple && !variableNames.includes(link.variableName)) {
-                variableNames.push(link.variableName);
-                res += toStringArray(links, link.variableName, output);
-            } else if(!link.multiple) {
-                res += `  ${link.variableName} = ${link.targetType}.${link.targetName}\n`;
-            }			
+		let ele = ""
+		if(output && link.representation === 'link') {
+      if(link.multiple && !variableNames.includes(link.variableName)) {
+          variableNames.push(link.variableName);
+          ele += toStringArray(links, link.variableName, output);
+      } else if(!link.multiple) {
+          ele += `  ${link.variableName} = ${link.targetType}.${link.targetName}\n`;
+      }
 		} else if(!output && link.representation === 'inverseLink') {
-            if(link.multiple && !variableNames.includes(link.variableName)) {
-                variableNames.push(link.variableName);
-                res += toStringArray(links, link.variableName, output);
-            } else if(!link.multiple) {
-                res += `  ${link.variableName} = ${link.sourceType}.${link.sourceName}\n`;
-            }		
-        }					
+      if(link.multiple && !variableNames.includes(link.variableName)) {
+          variableNames.push(link.variableName);
+          ele += toStringArray(links, link.variableName, output);
+      } else if(!link.multiple) {
+          ele += `  ${link.variableName} = ${link.sourceType}.${link.sourceName}\n`;
+      }
+    }
+		let found = false;
+
+		for (let object of objects) {
+			if (object.split('=')[0]?.trim() == link.variableName?.trim()) {
+				found = true
+			}
+		}
+		if (!found) {
+			res += ele
+		}
 	})
 	return res;
 }
 
-function toStringArray(resources, variableName, output, variableType) {	
+function toStringArray(resources, variableName, output, variableType) {
 	let res = `  ${variableName} = [`;
 	resources.forEach( resource => {
         if(variableName === resource.variableName && output) {
@@ -76,7 +88,7 @@ function toStringArray(resources, variableName, output, variableType) {
             res += `${resource.sourceType}.${resource.sourceName}, `;
         } else if(variableType !== undefined && variableType === resource.type) {
             res += `${resource.type}.${resource.name}, `;
-        } 
+        }
 	})
     res = res.slice(0, -2);
     res += ']\n';
@@ -84,7 +96,7 @@ function toStringArray(resources, variableName, output, variableType) {
 }
 
 
-function toStringContainer(resource, containers) {	
+function toStringContainer(resource, containers) {
 	let res = '';
     let variableTypes = [];
 
@@ -93,21 +105,21 @@ function toStringContainer(resource, containers) {
             if(attribute.representation === 'contained' || attribute.representation === 'containedInOtherContainer') {
                 containers.forEach( container => {
                     if(container.type === attribute.resourceType) {
-                        res += `  ${attribute.variableName} = ${container.type}.${container.name}\n`; 
+                        res += `  ${attribute.variableName} = ${container.type}.${container.name}\n`;
                     }
-                })  
+                })
                 if(!attribute.resourceType) {
-                    res += `  ${attribute.variableName} = ${containers[0].type}.${containers[0].name}\n`; 
-                }         
+                    res += `  ${attribute.variableName} = ${containers[0].type}.${containers[0].name}\n`;
+                }
             } else if(attribute.representation === 'contain') {
                 resource.contains.forEach( content => {
                     if(attribute.array && !variableTypes.includes(attribute.resourceType)) {
                         variableTypes.push(attribute.resourceType);
                         res += toStringArray(resource.contains, attribute.variableName, false, attribute.resourceType);
                     } else if(!attribute.array) {
-                        res += `  ${attribute.variableName} = ${content.type}.${content.name}\n`; 
+                        res += `  ${attribute.variableName} = ${content.type}.${content.name}\n`;
                     }
-                })               
+                })
             }
         })
     }
